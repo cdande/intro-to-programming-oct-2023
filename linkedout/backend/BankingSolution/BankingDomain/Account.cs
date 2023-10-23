0fac93d0f6431e07f5ff021f5373e0e00789ca4a
cdande <corbyn_anderson@yahoo.com>
2023-10-23 16:43:34 -0400

    Done with  Day one

---
+++ linkedout/backend/BankingSolution/BankingDomain/Account.cs
@@ -0,0 +1,22 @@
+﻿namespace Banking.Domain;
+
+public class Account
+{
+    private decimal _balance = 5000;
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+        //return 5000; "Slimed"
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        _balance -= amountToWithdraw;
+    }
+
+}
---
+++ linkedout/backend/BankingSolution/BankingSolution/NewAccounts.cs
@@ -0,0 +1,21 @@
+﻿using Banking.Domain;
+
+
+namespace BankingSolution;
+internal class NewAccounts
+{
+    [Fact]
+    public void NewAccountsHaveCorrectOpeningBalance()
+    {
+        // Write the Code You Wish You Had
+        // Given - I have a new account
+        Account account = new Account();
+
+        //when I ask it for it's balance
+        decimal openingBalance = account.GetBalance();
+
+        //then it is 5000 bucks. (decimal)
+        Assert.Equal(5000M, openingBalance);
+    }
+
+}
