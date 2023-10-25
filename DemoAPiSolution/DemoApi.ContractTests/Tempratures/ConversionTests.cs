56281929e20965c62cbcee76689de3b417c34f69
cdande <corbyn_anderson@yahoo.com>
2023-10-25 13:56:48 -0400

    before lunch on wednesday

---
+++ DemoAPiSolution/DemoApi.ContractTests/Tempratures/ConversionTests.cs
@@ -0,0 +1,58 @@
+﻿using Alba;
+
+
+
+namespace DemoApi.ContractTests.Temperatures;
+public class ConversionTests
+{
+
+
+
+	[Fact]
+	public async Task CanConvertFromFToC()
+	{
+
+		//Given 
+		var expectedResonpse = new ConversionResponse(212, 100);
+
+		var host = await AlbaHost.For<Program>();
+
+
+
+		var response = await host.Scenario(api =>
+		{
+			api.Get.Url("/temperatures/farenheit/212/celcius");
+			api.StatusCodeShouldBeOk();
+		});
+
+		var message = response.ReadAsJson<ConversionResponse>();
+
+		Assert.NotNull(message);
+
+		Assert.Equal(expectedResonpse, message);
+	}
+
+	[Fact]
+	public async Task CanConvertFromCtoF()
+	{
+
+		//Given 
+		var expectedResonpse = new ConversionResponse(100, 212);
+
+		var host = await AlbaHost.For<Program>();
+
+
+
+		var response = await host.Scenario(api =>
+		{
+			api.Get.Url("/temperatures/celcius/100/farenheit");
+			api.StatusCodeShouldBeOk();
+		});
+
+		var message = response.ReadAsJson<ConversionResponse>();
+
+		Assert.NotNull(message);
+
+		Assert.Equal(expectedResonpse, message);
+	}
+}
--- linkedout/backend/BankingSolution/Banking.Tests/MakingDeposits.cs
+++ linkedout/backend/BankingSolution/Banking.Tests/MakingDeposits.cs
@@ -5,19 +5,31 @@ namespace Banking.Tests;
 public class MakingDeposits
 {
 
-    [Fact]
-    public void MakingADepositIncreasesBalance()
-    {
-        // Given
-        var account = new Account();
-        var openingBalance = account.GetBalance();
-        var amountToDeposit = 100M;
+	[Fact]
+	public void MakingADepositIncreasesBalance()
+	{
+		// Given
+		var account = new Account();
+		var openingBalance = account.GetBalance();
+		var amountToDeposit = 100M;
 
-        // when
-        account.Deposit(amountToDeposit);
+		// when
+		account.Deposit(amountToDeposit);
 
 
-        // Then
-        Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
-    }
+		// Then
+		Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+	}
+
+	[Fact]
+
+	public void MultipleAccounts()
+	{
+		var bobsAccount = new Account();
+		var suesAccount = new Account();
+
+		Assert.Equal(5000, bobsAccount.GetBalance());
+		Assert.Equal(6000, suesAccount.GetBalance());
+
+	}
 }
