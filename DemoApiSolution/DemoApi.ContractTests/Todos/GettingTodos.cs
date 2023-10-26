78b0dcba6e079196a6ba1f1beee71c89833fbaa2
cdande <corbyn_anderson@yahoo.com>
2023-10-26 15:44:35 -0400

    Jeff's stuff

---
+++ DemoApiSolution/DemoApi.ContractTests/Todos/GettingTodos.cs
@@ -0,0 +1,24 @@
+﻿using Alba;
+namespace DemoApi.ContractTests.Todos;
+public class GettingTodos
+{
+
+	[Fact]
+	public async Task CanGetATodo()
+	{
+		var host = await AlbaHost.For<Program>();
+
+		await host.Scenario(api =>
+		{
+			api.Get.Url("/todo-list/808205e0-de44-493f-afa1-9bb8cfdbf3d1");
+			api.StatusCodeShouldBeOk();
+		});
+
+
+		await host.Scenario(api =>
+		{
+			api.Get.Url("/todo-list/" + Guid.NewGuid().ToString());
+			api.StatusCodeShouldBe(404);
+		});
+	}
+}
---
+++ stringcalculator-tuesday-jeff/StringCalculatorInterationTests.cs
@@ -0,0 +1,23 @@
+﻿
+namespace StringCalculator;
+public class StringCalculatorInterationTests
+{
+	private readonly StringCalculator _calculator;
+
+	public StringCalculatorInterationTests(StringCalculator calculator)
+	{
+		_calculator = calculator;
+	}
+
+	[Theory]
+	[InlineData("1")]
+	public void WritesToLogger(string numbers)
+	{
+		// when 
+		_calculator.Add(numbers);
+
+		// then
+		// Does "1" get written to the logger's write method?
+	}
+
+}
